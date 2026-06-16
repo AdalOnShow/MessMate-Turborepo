65fbdbd666001e51f3b0e390ea8ddc62d5836f05
Sharif Adal <sharifadal2008@gmail.com>
2026-06-16 20:50:23 +0600

    feat(turbo): update outputs in turbo.json and configure turbopack in next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,16 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
+};
 
 export default withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
